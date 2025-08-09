0dd9fddcd8c29cee468a4d7c324d275dc59eca15
Valeria-Jimenez27 <valej_27@hotmail.com>
2025-08-08 22:01:19 -0600

    Added week_13

---
+++ semana 13/exercise_3_decorator.py
@@ -0,0 +1,26 @@
+from datetime import date
+
+class User:
+    def __init__(self, birthdate):
+        self.birthdate = birthdate
+
+    @property
+    def age(self):
+        today = date.today()
+        return date.today().year - self.birthdate.year
+
+
+def adult(func):
+    def wrapper(user):
+        if user.age < 18:
+            return f"{user.age}: Access denied, is not an adult"
+        return func(user)
+    return wrapper
+
+@adult
+def enter_a_bar(user):
+    return f"{user.age}: You are allowed to enter"
+
+print(enter_a_bar(User(date(1998, 1, 27))))
+print(enter_a_bar(User(date(2008, 10, 7))))
+
