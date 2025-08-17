c17706e998db4a7356f6779ecfc987fccef7773c
Valeria-Jimenez27 <valej_27@hotmail.com>
2025-08-17 08:20:31 -0600

    added week_13

--- semana 13/exercise_1_decorator.py
+++ semana 13/exercise_1_decorator.py
@@ -1,14 +1,18 @@
 def my_first_decorator(func):
-    def wrapper(*args):
+    def wrapper(*args,**kwargs):
         print("Hello Roberto")
-        func(*args)
+        print("positional parameters:",*args)
+        print("keyword parameters:",**kwargs)
+        
+        result= func(*args,**kwargs)
+        print("result of the function:",result)
+        
         print("This is my first decorator!")
-
+        return result
     return wrapper
 
 @my_first_decorator
 def vale_decorator():
-    print("I never get the idea of decorators in python, but now I am")	
+    print("I had never understood the concept of decorators in Python, but now I do.")	
 
 vale_decorator()
-
