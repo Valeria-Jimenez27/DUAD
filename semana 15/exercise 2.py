6dc6d7ea59e48ee42c31ad533aad243de33aab12
Valeria-Jimenez27 <valej_27@hotmail.com>
2025-09-17 18:31:20 -0600

    Update exercise 2.py

--- semana 15/exercise 2.py
+++ semana 15/exercise 2.py
@@ -1,22 +1,25 @@
 def bubble_sort(list_of_sort):
-    for outer_index in range(0,len(list_of_sort) - 1):
+    n= len(list_of_sort)
+    for outer_index in range (0, n - 1):
+        print(f"Swapping: {outer_index +1}")
         changes_made=False
-        for index in range(0, len(list_of_sort)- 1 -outer_index):
-            current_number=list_of_sort[index]
-            next_number=list_of_sort[index+1]
-            print(f"Iteration {outer_index},current number {current_number}, next number {next_number}")
-            
-            if current_number < next_number:
-                print("Swapping numbers")
-                list_of_sort[index]=next_number
-                list_of_sort[index+1]=current_number
-                changes_made=True
-            
-            
-        if not changes_made:
-            return
+        
+        for index in range(n - 1, outer_index, -1):
+            current_number = list_of_sort[index - 1]
+            next_number = list_of_sort[index]
+            if next_number > current_number:
+                print(f"Swapping numbers")
+                list_of_sort[index - 1], list_of_sort[index] = next_number, current_number
+                changes_made = True
+            else:
+                print("No exchange of numbers")
 
-testing_list=[23,97,2,80,10,73.2,-100]
+        print(f"Iteration {outer_index + 1}: {list_of_sort}")
+
+        if not changes_made:
+            break
+
+testing_list=[1,2,3,4]
 bubble_sort(testing_list)
 
 
