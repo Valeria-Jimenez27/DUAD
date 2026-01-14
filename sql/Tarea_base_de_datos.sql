e3a0beaa2dd29747364ebec8f014ece9998a848d
Valeria-Jimenez27 <valej_27@hotmail.com>
2026-01-14 11:05:36 -0600

    added sql_practice

--- sql/Tarea_base_de_datos.sql
+++ sql/Tarea_base_de_datos.sql
@@ -1,9 +1,21 @@
-UPDATE bills
-SET total_amount = CASE pk_id_bill
-    WHEN 335455 THEN 150
-END
-WHERE pk_id_bill IN (335455);
+-- List all tables in the database
+SELECT name FROM sqlite_master WHERE type='table';
+-- Get the schema of the all tables
+SELECT sql
+FROM sqlite_master
+WHERE type='table' AND name='products';
 
+SELECT sql
+FROM sqlite_master
+WHERE type='table' AND name='bills';
+
+SELECT sql
+FROM sqlite_master
+WHERE type='table' AND name='bill_details';
+
+SELECT sql
+FROM sqlite_master
+WHERE type='table' AND name='shopping_cart';
 
 
 
