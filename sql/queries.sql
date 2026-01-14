d8a3d461645f38057fb33326bc8a0af8dc4e2e68
Valeria-Jimenez27 <valej_27@hotmail.com>
2026-01-14 11:54:58 -0600

    sql_practice

--- sql/queries.sql
+++ sql/queries.sql
@@ -5,19 +5,18 @@ SELECT *
 FROM products
 WHERE price > 50000;
 
-SELECT *
+SELECT fk_product_id, amount
 FROM bill_details
-WHERE pk_detail_id= 335455;
+WHERE fk_product_id = 3456;
 
-SELECT 
-    fk_product_id,
-    SUM(amount) as total_amount
+SELECT fk_product_id,
+SUM(amount) as total_amount
 FROM bill_details
 GROUP BY fk_product_id;
 
 SELECT *
-FROM bill_details
-WHERE pk_detail_id = 2;
+FROM bills
+WHERE email_customers = 'hjejh34@gmail.com';
 
 SELECT *
 FROM bills
