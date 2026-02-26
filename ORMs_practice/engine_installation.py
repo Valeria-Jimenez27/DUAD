1bd609724d85335f81f356c24c59bb398663c9be
Valeria-Jimenez27 <valej_27@hotmail.com>
2026-02-25 20:27:16 -0600

    Modified engine_installation.py

--- ORMs_practice/engine_installation.py
+++ ORMs_practice/engine_installation.py
@@ -2,13 +2,20 @@ from sqlalchemy import create_engine
 from Manage_data import Base
 
 DB_URI = "postgresql://postgres:postgres@localhost:5432/orms_practice"
-
 engine = create_engine(DB_URI, echo=True)
 
-try:
-    with engine.connect() as connection:
-        print("Connection successful!")
-    Base.metadata.create_all(engine)
-    print("Tables created successfully!")
-except Exception as e:
-    print("Connection failed:", e)
+
+def test_connection():
+    try:
+        with engine.connect() as connection:
+            print("Connection successful!")
+
+        Base.metadata.create_all(engine)
+        print("Tables created successfully!")
+
+    except Exception as e:
+        print("Connection failed:", e)
+
+
+if __name__ == "__main__":
+    test_connection()
