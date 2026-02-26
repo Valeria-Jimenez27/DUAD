3c6762d313847c09befb88fff54fdc8c496d4e20
Valeria-Jimenez27 <valej_27@hotmail.com>
2026-02-25 20:18:15 -0600

    Remove classical ORMs file

--- ORMs_practice/engine_installation.py
+++ ORMs_practice/engine_installation.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
-
+from Manage_data import Base
 
 DB_URI = "postgresql://postgres:postgres@localhost:5432/orms_practice"
+
 engine = create_engine(DB_URI, echo=True)
 
 try:
-    connection = engine.connect()
-    print("Connection successful!")
-    connection.close()
+    with engine.connect() as connection:
+        print("Connection successful!")
+    Base.metadata.create_all(engine)
+    print("Tables created successfully!")
 except Exception as e:
     print("Connection failed:", e)
